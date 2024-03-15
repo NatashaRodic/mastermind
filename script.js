217816f0f3a2d40597f3302d2fbd64af2d95a9a6
Natasha Rodic <natashawolt@gmail.com>
2024-03-15 14:03:01 -0400

    Decalare constants and state varibales

---
+++ script.js
@@ -0,0 +1,21 @@
+/*----- constants -----*/
+const MAX_ATTEMPTS = 10;
+const COLOR_PICKED = ['red', 'purple', 'blue', 'green', 'yellow', 'gray']
+
+
+/*----- state variables -----*/
+let playersBoard = [];
+let feedbackBoard = [];
+let currentGuess;
+let result;
+let winCombo;
+let sampleBoard;
+
+/*----- cached elements  -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
