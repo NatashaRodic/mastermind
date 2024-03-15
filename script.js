72dcea791740acf5243811acc27d5be99c659f91
Natasha Rodic <natashawolt@gmail.com>
2024-03-15 18:42:01 -0400

    Add init function and cached elements

--- script.js
+++ script.js
@@ -12,10 +12,24 @@ let winCombo;
 let sampleBoard;
 
 /*----- cached elements  -----*/
-
-
+const message = document.getElementById('message');
+const button = document.querySelector('.button')
+const samples = [...document.querySelectorAll('#sample-board > div')]
+console.log(samples)
 
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
+init();
+
+function init() {
+
+    playersBoard = [[null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null]];
+    feedbackBoard = [[null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null], [null, null, null, null]];
+    currentGuess = 0;
+    result = null;
+    sampleBoard = null,
+        render()
+
+}
